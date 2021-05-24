549510e022902544b25bdaf7d92bf7aadfbb1440
madan <madan9>
2021-05-24 17:55:57 +0545

    added Header component by madan branch to test merge in git

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>
+        i'm header component added by madan branch to test merge from madan into
+        main/master branch lets go
+      </p>
+    </div>
+  );
+};
+
+export default Header;
